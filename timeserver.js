ed78ce197e2dd4c723085bba80a1bafa69f84af3
Danny Peck <dannytpeck@gmail.com>
2015-09-16 14:51:10 -0700

    Finish time server

---
+++ timeserver.js
@@ -0,0 +1,9 @@
+var net = require('net')
+var strftime = require('strftime')
+
+var port = Number(process.argv[2])
+
+var server = net.createServer(function (socket) {
+  socket.end(strftime('%Y-%m-%d %H:%M'))
+})
+server.listen(port)
